1a9b9e91f89a2155761bece0d9f9d2cf5ef8e94d
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-04-24 14:16:02 -0700

    Added variables to tests for Engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,10 +1,14 @@
-const Employee = require(`../lib/Employee`)
 const Engineer = require(`../lib/Engineer`)
 
 // Initialize tests for the Engineer Class
 describe(`Engineer`, () => {
     // Create a new Engineer object, with a new github arguement
-    engineer = new Engineer(`Kevin`, 5, `derp@gmail.com`, `kh288`);
+    const name = `Kevin`;
+    const id = 5;
+    const email = `derp@gmail.com`;
+    const github = `kh288`;
+
+    engineer = new Engineer(name, id, email, github);
     it(`Creating an engineer should be an object`, () => {
         expect(typeof engineer).toEqual(`object`);
     })
@@ -12,19 +16,19 @@ describe(`Engineer`, () => {
     describe(`getName`, () => {
         it(`should return the name that has been set for the "name" arguement`, () => {
             // We expect the getName function to return `Kevin`
-            expect(engineer.getName()).toEqual(`Kevin`);
+            expect(engineer.getName()).toEqual(name);
         })
     })
     describe(`getId`, () => {
         it(`should return the Id that has been set for the "id" arguement`, () => {
             // We expect the getId function to return `5`
-            expect(engineer.getId()).toEqual(5);
+            expect(engineer.getId()).toEqual(id);
         })
     })
     describe(`getEmail`, () => {
         it(`should return the email that has been set for the "email" arguement`, () => {
             // We expect the getEmail function to return `derp@gmail.com`
-            expect(engineer.getEmail()).toEqual(`derp@gmail.com`);
+            expect(engineer.getEmail()).toEqual(email);
         })
     })
     describe(`getRole`, () => {
@@ -35,7 +39,8 @@ describe(`Engineer`, () => {
     })
     describe(`getGithub`, () => {
         it(`should return the value of their github username`, () => {
-            expect(egineer.getGithub().toEqual(`https://github.com/kh288`))
+            // We expect the github username to be added to a link and `getGithub()` is called
+            expect(egineer.getGithub().toEqual(`https://github.com/${github}`))
         })
     })
 })
